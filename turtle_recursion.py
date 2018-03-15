0ae762c2a3ca56bee5867434dbd9b2c40af3af1f
simoneturner <sturner18@fwparker.org>
2018-03-15 14:25:28 -0500

    March 6

--- turtle_recursion.py
+++ turtle_recursion.py
@@ -2,11 +2,56 @@
 Turtle Recursion (33pts)
 
 1)  Using the turtle library, create the H fractal pattern according to the file shown in the data folder. (15pts)
+my_screen.clear()
+'''
+import turtle
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)
+my_turtle.shape("turtle")
+my_turtle.showturtle()
+my_screen = turtle.Screen()
+my_screen.bgcolor('white')
 
+def recursive_h(x, y, size, depth):
+    if depth > 0:
+        my_turtle.up()
+        my_turtle.goto(x, y)
+        my_turtle.down()
+        my_turtle.goto(x + (size / 2), y)
+        my_turtle.goto(x + (size / 2), y + (size / 2))
+        my_turtle.goto(x + (size / 2), y - (size / 2))
+        my_turtle.up()
+        my_turtle.goto(x, y)
+        my_turtle.down()
+        my_turtle.goto(x - (size / 2), y)
+        my_turtle.goto(x - (size / 2) , y - (size / 2))
+        my_turtle.goto(x - (size / 2), y + (size / 2))
+        recursive_h(x + (size / 2), y + (size / 2), (size / 2), depth - 1)
+        recursive_h(x + (size / 2) , y - (size / 2), (size / 2), depth - 1)
+        recursive_h(x - (size / 2), y + (size / 2), (size / 2), depth - 1)
+        recursive_h(x - (size / 2), y - (size / 2), (size / 2), depth - 1)
+
+recursive_h(100, 100, 100, 4)
+
+my_screen.exitonclick()  # end of program
+
+'''
 2)  Using the turtle library, create any of the other recursive patterns in the data folder. 
 Challenge yourself to match your pattern as closely as you can to the image.  (15pts)
 Note:  The Koch snowflake shows step by step building of the fractal.  
        The rectangle fractal example shows 4 possible drawings of the same fractal (choose any one)
+       
+'''
+
+def recursive_snowflake(x, y, size, depth):
+    if depth > 0:
+        my_turtle.up()
+        my_turtle.goto(x, y)
+        my_turtle.down()
+
+
+
+'''
 
 3)  Create your own work of art with a repeating pattern of your making.  
 It must be a repeated pattern using recursion. Snowflakes, trees, and spirals are a common choice.  
