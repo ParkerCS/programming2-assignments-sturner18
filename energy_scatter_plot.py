1645a71205d420a14b9cda114c80dbbcec7273ed
simoneturner <sturner18@fwparker.org>
2018-03-29 14:28:11 -0500

    Merge branch 'master' of http://github.com/fwparkercode/Programming2
    # Conflicts:
    #	plotting_data_problems.py

--- energy_scatter_plot.py
+++ energy_scatter_plot.py
@@ -1,10 +1,80 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
 '''
 Energy Efficiency of Chicago Schools (35pts)
-
+import csv
+import matplotlib.pyplot as plt
+import numpy as np 
 https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2016-Data-Reported-in-/fpwt-snya\
+'''
 
+with open("files/Chicago_Energy_Benchmarking_-_2016_Data_Reported_in_2017.csv") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+print(data)
+schools_to_plot = []
+
+new_data = []
+for i in range(len(data)):
+    try:
+        ghg_intensity = float(data[i][21])
+        new_data.append(data[i])
+    except:
+        pass
+
+data = new_data[:]
+
+for i in range(len(data)):
+    if data[i][6] == "K-12 School" and data[i][21] != " ":
+            schools_to_plot.append(data[i])
+
+schools_to_plot.sort(key=lambda x: float(x[21]))
+
+print(schools_to_plot)
+print(len(schools_to_plot))
+
+ghg_emissions = []
+square_footage = []
+school_names = []
+
+for i in range(len(schools_to_plot)):
+        try:
+            emissions = float(schools_to_plot[i][20])
+            ghg_emissions.append(emissions)
+            footage = float(schools_to_plot[i][7])
+            square_footage.append(footage)
+            names = schools_to_plot[i][2]
+            if names == "Francis W Parker School" or i < 5 or i > (len(schools_to_plot) - 5):
+                school_names.append(names)
+            else:
+                school_names.append("")
+        except:
+             print(data[i][0], "has no data.")
+
+plt.figure(1, figsize=(12, 7))
+
+plt.scatter(square_footage, ghg_emissions, s=4, c="red")
+plt.title("Energy Efficiency of Chicago Schools")
+plt.xlabel("School Square Footage")
+plt.ylabel("Green House Gas Emissions")
+
+m, b = np.polyfit(square_footage, ghg_emissions, 1) # 1 for linear, returns slope and y intercept
+
+x = [0, 700000]
+y = [m * point + b for point in x]
+plt.plot(x, y, color='blue')
+
+for i in range(len(school_names)):
+    plt.annotate(school_names[i], xy=(square_footage[i], ghg_emissions[i])) # text, xy =()
+
+plt.show()
+
+'''
 Chicago requires that all buildings over 50000 square feet in the city comply with energy benchmark reporting each year.
-The dataset at the link above is that data from 2016 which was reported in 2017.
+The data set at the link above is that data from 2016 which was reported in 2017.
 
 We will use this data to look at schools.  We will visualize the efficiency of schools by scatter plot.  
 We expect that the more square footage (sqft) a school is, the more greenhouse gas (ghg) emission it will produce.
