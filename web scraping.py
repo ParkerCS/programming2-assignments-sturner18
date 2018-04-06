3a583a030d39cc89c88cc8d31dcf02778612c023
simoneturner <sturner18@fwparker.org>
2018-04-06 08:40:55 -0500

    Merge branch 'master' of http://github.com/fwparkercode/Programming2
    # Conflicts:
    #	plotting_data_problems.py

--- web scraping.py
+++ web scraping.py
@@ -41,34 +41,49 @@ page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 
 days_list = []
-days = soup.findAll(class_="twc-sticky-col")
+days = soup.findAll(class_="date-time")
 for day in days:
     days_list.append(day.text.strip())
+
+dates_list = []
+dates = soup.findAll(class_="day-detail")
+for date in dates:
+    dates_list.append(date.text.strip())
 
 descriptions_list = []
 descriptions = soup.findAll(class_="description")
 for description in descriptions:
     descriptions_list.append(description.text.strip())
 
+descriptions_list = descriptions_list[1:]
+
 temps_list = []
 temps = soup.findAll(class_="temp")
 for temp in temps:
     temps_list.append(temp.text.strip())
+
+temps_list = temps_list[1:]
 
 percips_list = []
 precips = soup.findAll(class_="precip")
 for precip in precips:
     percips_list.append(precip.text.strip())
 
+percips_list = percips_list[1:]
+
 winds_list = []
 winds = soup.findAll(class_="wind")
 for wind in winds:
     winds_list.append(wind.text.strip())
+
+winds_list = winds_list[1:]
 
 humidity_list = []
 humidity = soup.findAll(class_="humidity")
 for hum in humidity:
     humidity_list.append(hum.text.strip())
 
+humidity_list = humidity_list[1:]
+
 for i in range(len(days_list)):
-    print("\n", days_list[i] + ",", "will be", descriptions_list[i], "with a", percips_list[i], "of precipitation" + ".", "The wind will be traveling", winds_list[i], "and a", humidity_list[i], "humidity, and a temperature of", temps_list[i] + ".", "\n")
+    print("\n", days_list[i] + ",", dates_list[i],  "will be", descriptions_list[i], "with", percips_list[i], "chance of precipitation" + ".", "The wind will be traveling", winds_list[i], "with a", humidity_list[i], "humidity and a high/low temperature of", temps_list[i] + ".", "\n")
